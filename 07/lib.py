204d854952860157a70b9370aed108f6b78b3073
nyghtly-derek <derekblankenship05@gmail.com>
2018-08-08 12:15:36 -0400

    solved 7b

--- 07/lib.py
+++ 07/lib.py
@@ -8,6 +8,7 @@ class Program:
         self.holding_programs = False
         self.is_base = True
         self.held_by = ""
+        self.true_weight = 0
     
     def give_program_to_hold(self, program_name):
         self.programs_held.append(program_name)
@@ -16,6 +17,9 @@ class Program:
     def set_held_by(self, program_name):
         self.held_by = program_name
         self.is_base = False
+    
+    def set_true_weight(self, true_weight):
+        self.true_weight = true_weight
 
     def is_holding_programs(self):
         return self.holding_programs
@@ -26,8 +30,11 @@ class Program:
     def get_programs_held(self):
         return self.programs_held
     
-    def get_weight(self):
-        return self.weight
+    def get_weight(self, get_true=False):
+        if get_true == True:
+            return self.true_weight
+        else:
+            return self.weight
     
     def get_name(self):
         return self.name
@@ -41,14 +48,17 @@ class Program:
     def __str__(self):
         return ("\tProgram: {}"
                 "\n\tWeight: {}"
+                "\n\tTrue Weight: {}"
                 "\n\tHolding: {}"
-                "\n\tHeld By: {}").format(self.name, self.weight, self.programs_held, self.held_by)
+                "\n\tHeld By: {}").format(self.name, self.weight, self.true_weight, self.programs_held, self.held_by)
 
 def build_program_tower(dataset):
     tower = {}
     prog_heldby_pairs = []
     for data in dataset:
-        newprogram = Program(data[0], data[1])
+        name = data[0]
+        weight = int(data[1])
+        newprogram = Program(name, weight)
         if '->' in data:
             i = 3
             while i < len(data):
@@ -61,7 +71,55 @@ def build_program_tower(dataset):
         tower[newprogram.get_name()] = newprogram
     for pair in prog_heldby_pairs:
         tower[pair[0]].set_held_by(pair[1])
+    calc_total_weight(tower) # recursively sets true weight value for all programs in tower
     return tower
+
+def calc_total_weight(tower):
+    base = find_base(tower)
+    return find_true_weight(base.get_name(), tower)
+
+def find_true_weight(name, tower):
+    if tower[name].get_weight(True) != 0:
+        return tower[name].get_weight(True)
+    elif not tower[name].is_holding_programs():
+        tower[name].set_true_weight(tower[name].get_weight())
+        return tower[name].get_weight(True)
+    else:
+        t_weight = 0
+        for held_name in tower[name].get_programs_held():
+            t_weight += find_true_weight(held_name, tower)
+        tower[name].set_true_weight(t_weight + tower[name].get_weight())
+        return tower[name].get_weight(True)
+
+def is_imbalanced(name, tower):
+    """ 
+    checks if program with `name` in a `tower` is imbalanced 
+    returns the difference between the programs weight and what it should weigh
+    (i.e. return value will be 0 if program is balanced)
+    """
+    if tower[name].is_base_case():
+        return 0
+    held_by = tower[name].get_held_by()
+    neighbors = tower[held_by].get_programs_held()
+    true_weights = []
+    weights = {}
+    count = {}
+    for program in neighbors:
+        weight = tower[program].get_weight()
+        weights[program] = weight
+        true_weight = tower[program].get_weight(True)
+        true_weights.append(true_weight)
+        if true_weight in count:
+            count[true_weight] += 1
+        else:
+            count[true_weight] = 1
+    most_common_weight = 0
+    frequency = 0
+    for true_weight in count:
+        if count[true_weight] > frequency:
+            most_common_weight = true_weight
+            frequency = count[true_weight]
+    return most_common_weight - tower[name].get_weight(True)
 
 def print_tower(tower):
     print("Program Tower:")
--- 07/recur.py
+++ 07/recur.py
@@ -46,6 +46,25 @@ tknk --- padx - havc
 In this example, tknk is at the bottom of the tower (the bottom program), and is holding up ugml, padx, and fwft. Those programs are, in turn, holding up other programs; in this example, none of those programs are holding up any other programs, and are all the tops of their own towers. (The actual tower balancing in front of you is much larger.)
 
 Before you're ready to help them, you need to make sure your information is correct. What is the name of the bottom program?
+
+--- Problem 7B ---
+
+The programs explain the situation: they can't get down. Rather, they could get down, if they weren't expending all of their energy trying to keep the tower balanced. Apparently, one program has the wrong weight, and until it's fixed, they're stuck here.
+
+For any program holding a disc, each program standing on that disc forms a sub-tower. Each of those sub-towers are supposed to be the same weight, or the disc itself isn't balanced. The weight of a tower is the sum of the weights of the programs in that tower.
+
+In the example above, this means that for ugml's disc to be balanced, gyxo, ebii, and jptl must all have the same weight, and they do: 61.
+
+However, for tknk to be balanced, each of the programs standing on its disc and all programs above it must each match. This means that the following sums must all be the same:
+
+    ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+    padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+    fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
+
+As you can see, tknk's disc is unbalanced: ugml's stack is heavier than the other two. Even though the nodes above ugml are balanced, ugml itself is too heavy: it needs to be 8 units lighter for its stack to weigh 243 and keep the towers balanced. If this change were made, its weight would be 60.
+
+Given that exactly one program is the wrong weight, what would its weight need to be to balance the entire tower?
+
 """
 import re
 import lib
@@ -54,7 +73,7 @@ def build_data(file):
     data = []
     for line in file:
         program_data = line.split()
-        regex = re.compile('[,]')
+        regex = re.compile('[,()]')
         for i in range(len(program_data)):
             program_data[i] = regex.sub('', program_data[i])
         data.append(program_data)
@@ -68,5 +87,32 @@ path = "data/myinput.txt"
 myfile = open(path, "r")
 data = build_data(myfile)
 tower = lib.build_program_tower(data)
-lib.print_tower(tower)
-print("\nbase of tower is", lib.find_base(tower).get_name())
+#lib.print_tower(tower)
+base = lib.find_base(tower)
+print("\n--- Solving for dataset {}: ---".format(path))
+print("building tower...")
+print("finding base...")
+print("\tBase of tower:\t", base.get_name())
+
+print("\ntesting balace...")
+imbas = []
+for program in tower:
+    imba = lib.is_imbalanced(program,tower)
+    if imba != 0:
+        imbas.append((program, imba))
+
+print("\nList of imbas:")
+for imba in imbas:
+    print("\t{}\t{}".format(imba[0], imba[1]))
+
+print("\nSearching for root imbalance...")
+root_imba = tower[imbas[0][0]]
+diff = imbas[0][1]
+for imba in imbas:
+    if tower[imba[0]].get_weight(True) < root_imba.get_weight(True):
+        root_imba = tower[imba[0]]
+        diff = imba[1]
+
+print("\tRoot imbalance:\t", root_imba.get_name())
+print("\tCurrent weight:\t", root_imba.get_weight())
+print("\tTarget weight:\t", root_imba.get_weight() + diff)
